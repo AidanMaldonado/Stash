78ce327022049574ed4be246818840089da4e97e
Aidan Maldonado <aidanm1198@gmail.com>
2018-12-26 13:13:06 -0500

    Added UI
    Finalized more UI elements
    Added Icons

--- Stash/ViewController.swift
+++ Stash/ViewController.swift
@@ -29,11 +29,13 @@ class ViewController: UIViewController {
         }
     }
 
-        
-
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        updateLabels()
+    }
+    
+    func updateLabels() {
+        moneyLabel.text = "$" + String(money)
     }
 
 
